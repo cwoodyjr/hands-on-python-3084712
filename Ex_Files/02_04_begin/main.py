7017ed7e76460182ae935bb266afd7e389f41f60
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2022-12-28 00:51:17 +0000

    looking at loops

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -2,12 +2,17 @@ NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
 i = 0
+print("This is a while loop:")
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
     i += 1
 
+print("This is a for in (for each) Loop")
+
 for name in NAMES:
     print(name)
+
+print("This is a for zip loop ")
 
 for name, age in zip(NAMES, AGES):
     print(f"{name} {age}")
@@ -19,3 +24,7 @@ for i in range(5):
     print(i)
 
 # enumerate
+#this will iterate through the list and print
+#the index number with the item
+for i, name in enumerate(NAMES):
+    print(f"{i} {name}")
