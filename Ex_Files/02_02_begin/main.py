9c51c44c7163e7c10701d138e24a5bebc56bcbdd
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2022-12-28 00:09:14 +0000

    playing with strings and formats

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -2,11 +2,13 @@ greet = "Hello World"
 extened_grt = "Hello World, " + "this is a long string"
 
 name = "John"
+sname = "Harris"
 
-intrupution = f"Hello {name}"
+intrupution = f"Hello {name} {sname}"
 
 greet_format = "Hello {}"
 
-formatted = greet_format.format(name)
+formatted = greet_format.format(name+" "+ sname)
 
-print(intrupution, formatted)
+print(intrupution, formatted.upper())
+print(intrupution.lower())
