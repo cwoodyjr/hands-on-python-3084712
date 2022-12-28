13dd21f9d91742bd78ccd958e49e92c307a691c6
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2022-12-28 00:22:35 +0000

    lists and slicing run through

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,11 +4,12 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
-EVERY_OTHER = NAMES[::2]
+JOHN_PAUL = NAMES[:2] #prints everything before index 2 (3rd item)
+GEORGE_RINGO = NAMES[2:] #prints everything after index 2 (3rd item)
+REVERSE = NAMES[::-1]   #start point = 0, end point = 0, step -1 (backwards), so print the list backwards
+EVERY_OTHER = NAMES[::2] #start point = 0, end point = 0, step by 2, so print every other item
 
+# Theses lines use built in functions to calculate:
 print(sum(AGES))
 print(min(AGES))
 print(max(AGES))
