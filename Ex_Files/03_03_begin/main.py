23c13fcf2aa4dc014194103923aa4384b7bbfc04
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2023-01-03 11:09:55 +0000

    converting csv to json

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -11,15 +11,16 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-einstein_json = json.dumps(EINSTEIN)
-back_to_dict = json.loads(einstein_json)
+einstein_json = json.dumps(EINSTEIN) #converts csv dict to json
+back_to_dict = json.loads(einstein_json) #converts json back to dict
 print(einstein_json)
 pprint(back_to_dict)
 
-with open("laureates.csv", "r") as f:
-    reader = csv.DictReader(f)
-    laureates = list(reader)
+with open("laureates.csv", "r") as f: #brings in file in read mode
+    reader = csv.DictReader(f) #creates a dict reader using f 
+    laureates = list(reader) #create a list of dict items, each represents a laureate
 
 
-with open("laureates.json", "w") as f:
-    json.dump(laureates, f, indent=2)
+with open("laureates.json", "w") as f: #opens file in write mode 
+    json.dump(laureates, f, indent=2) #using json.dump(no 's') to create json file as f,
+                                      #setting indent spaces to 2
